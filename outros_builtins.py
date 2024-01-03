9e2aa48506b0f8b5d626f9f0971eec2f918c0fd7
emmanoel-amunik <emmanoelamunik@hotmail.com>
2024-01-03 19:54:08 -0300

    05 Outros Builtins

---
+++ outros_builtins.py
@@ -0,0 +1,22 @@
+ages = [15, 87, 32, 65, 56, 32, 49, 37]
+
+for index in range(len(ages)):
+    print(index, '-', ages[index])
+
+
+for value in enumerate(ages):  # packed
+    print(value)
+
+
+for index, value in enumerate(ages):  # unpacking
+    print(index, "x", value)
+
+
+users = [
+    ("Guilherme", 37, 1981),
+    ("Daniela", 31, 1987),
+    ("Paulo", 39, 1979)
+]
+for name, age, birth in users:  # unpacked
+    print(name)
+
